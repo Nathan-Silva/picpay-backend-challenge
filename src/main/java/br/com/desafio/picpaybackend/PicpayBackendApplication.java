e9867ef59e2af6f46f398c67cc04e9bba167ea2a
nathanbraga <braganathan38@gmail.com>
2024-04-14 23:49:06 -0300

    init: Initial commit

---
+++ src/main/java/br/com/desafio/picpaybackend/PicpayBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.picpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/picpaybackend/PicpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.picpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
